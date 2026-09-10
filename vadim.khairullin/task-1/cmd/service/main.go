ab1ab81b095dea82f34540189de567f9086f1d93
BinaryKenny <khairullinvadim2006@gmail.com>
2026-09-10 21:14:38 +0300

    [TASK-1] Inputting all variables and checking its error

--- vadim.khairullin/task-1/cmd/service/main.go
+++ vadim.khairullin/task-1/cmd/service/main.go
@@ -11,4 +11,21 @@ func main() {
 		fmt.Println("Invalid first operand")
 		return
 	}
+
+	_, err2 := fmt.Scan(&number2)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err3 := fmt.Scan(&operator)
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	if operator == '/' && number2 == 0 {
+		fmt.Println("Division by zero")
+		return
+	}
 }
