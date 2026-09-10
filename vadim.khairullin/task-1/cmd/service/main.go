3efb7e2c5e4c515e9dbde2a3b9d460dc91d2787a
BinaryKenny <khairullinvadim2006@gmail.com>
2026-09-10 22:45:13 +0300

    [TASK-1] Fix case '/': if the second number is zero it finishes the program

--- vadim.khairullin/task-1/cmd/service/main.go
+++ vadim.khairullin/task-1/cmd/service/main.go
@@ -11,13 +11,11 @@ func main() {
 		fmt.Println("Invalid first operand")
 		return
 	}
-
 	_, err2 := fmt.Scan(&number2)
 	if err2 != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-
 	_, err3 := fmt.Scan(&operator)
 	if err3 != nil {
 		fmt.Println("Invalid operation")
@@ -34,6 +32,7 @@ func main() {
 	case "/":
 		if number2 == 0 {
 			fmt.Println("Division by zero")
+			return
 		}
 		fmt.Println(number1 / number2)
 	default:
