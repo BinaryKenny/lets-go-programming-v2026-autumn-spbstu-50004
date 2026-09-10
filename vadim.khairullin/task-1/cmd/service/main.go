893cc4ce2dcb1bbba1f79270455cd71d285a858d
BinaryKenny <khairullinvadim2006@gmail.com>
2026-09-10 21:13:14 +0300

    [TASK-1] Declaring the main variables

--- vadim.khairullin/task-1/cmd/service/main.go
+++ vadim.khairullin/task-1/cmd/service/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var number1, number2 int
+	var operator rune
+
+	_, err1 := fmt.Scan(&number1)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
 }
